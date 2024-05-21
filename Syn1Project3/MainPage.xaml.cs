554e77cc60795917e9b7cf5b0b29d3df34ee0f79
miimoller <bhaktarohan20@gmail.com>
2024-05-21 07:57:27 -0700

    Add project files.

---
+++ Syn1Project3/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Syn1Project3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Syn1Project3/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Syn1Project3
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
